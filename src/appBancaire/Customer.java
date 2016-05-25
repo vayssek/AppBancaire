c9783572a18c74f83c2fff7d7a51a110f216f6b4
Kevin Vaysse <vayssek@gmail.com>
2016-05-25 10:27:23 +0200

    ADD class attribut for Customer class

--- src/appBancaire/Customer.java
+++ src/appBancaire/Customer.java
@@ -1,5 +1,28 @@
 package appBancaire;
 
 class Customer {
+	
+	private String lastName;
+	private String firstName;
+	private String completeName;
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getCompleteName() {
+		return completeName;
+	}
+	public void setCompleteName(String completeName) {
+		this.completeName = completeName;
+	}
 
 }
