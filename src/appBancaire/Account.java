e5cfa98d8f3676a9cc969e785d25651282c6e431
Kevin Vaysse <vayssek@gmail.com>
2016-05-25 10:21:02 +0200

    ADD class attribut in Account class
    Attribut : number (String) and balance(double)

--- src/appBancaire/Account.java
+++ src/appBancaire/Account.java
@@ -1,5 +1,21 @@
 package appBancaire;
 
 class Account {
-
+	
+	private String number;
+	private double balance;
+	
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
 }
